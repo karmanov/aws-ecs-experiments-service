2cbfb916477b803552259378b4286c33b73f94df
Dmytro Karmanov <dkarmanov@wayfair.com>
2023-01-21 08:47:36 +0100

    Initial commit

---
+++ src/main/java/io/karmanov/lab/AwsEcsExperimentsServiceApplication.java
@@ -0,0 +1,13 @@
+package io.karmanov.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEcsExperimentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsEcsExperimentsServiceApplication.class, args);
+	}
+
+}
